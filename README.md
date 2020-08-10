6c20e10004ee900451e7c2c1f46bfac2ce8f2fe0
William Lindvall <shwilliam@hey.com>
2020-08-10 13:47:06 -0400

    init

---
+++ README.md
@@ -0,0 +1,10 @@
+# Design Patterns in JavaScript
+
+## Patterns
+
+- Strategy
+
+## References
+
+- [Head First Design Patterns](https://ce.sharif.edu/courses/98-99/2/ce484-1/resources/root/Design Patterns/Eric Freeman, Elisabeth Freeman, Kathy Sierra, Bert Bates-Head First Design Patterns -OReilly (2008).pdf) by Eric Freeman, Elisabeth Freeman, Kathy Sierra & Bert Bates
+- [Design Patterns: Elements of Reusable Object-Oriented Software](https://www.uml.org.cn/c++/pdf/DesignPatterns.pdf) by Erich Gamma, Richard Helm, Ralph Johnson & John Vlissides
---
+++ patterns/behavioral/strategy.js
@@ -0,0 +1,48 @@
+/*
+ * Strategy Pattern
+ * ================
+ *
+ * A family of interchangeable algorithms that can vary independently from
+ * the clients that use them
+ *
+ */
+
+class StrategyA {
+  doThing() {
+    console.log('a')
+  }
+}
+
+class StrategyB {
+  doThing() {
+    console.log('b')
+  }
+}
+
+class StrategyManager {
+  constructor({strategy = null}) {
+    this._strategy = strategy
+  }
+
+  set strategy(strategy) {
+    this._strategy = strategy
+  }
+
+  get strategy() {
+    return this._strategy
+  }
+
+  doThing() {
+    this._strategy.doThing()
+  }
+}
+
+const stratA = new StrategyA()
+const stratB = new StrategyB()
+const stratManager = new StrategyManager({strategy: stratA})
+
+stratManager.doThing() // a
+
+stratManager.strategy = stratB
+
+stratManager.doThing() // b
